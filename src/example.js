c5dec4e6001e682a545a8a59849ec7a51cc6290b
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2024-04-21 20:51:08 -0500

    Add example.js file for testing

---
+++ src/example.js
@@ -0,0 +1,41 @@
+import terminal from "./core/terminal.js";
+import ps from "prompt-sync";
+
+const prompt = ps();
+const data = {
+  playing: true,
+};
+
+function lookCommand(parserObj) {
+  if (parserObj.args.length == 0)
+    return console.log("You take in your surroundings.");
+  const thing = parserObj.args[0];
+
+  console.log("You look at the " + thing);
+}
+
+function quitCommand(parserObj, data) {
+  console.log("Are you sure you want to quit?\ny\\n");
+  if (/y/i.test(prompt())) data.playing = false;
+}
+
+const commandList = {
+  l: lookCommand,
+  quit: quitCommand,
+};
+
+function game() {
+  console.clear();
+  console.log(
+    "You spawn into the world. Use the prompt to interact with the world.",
+  );
+
+  while (data.playing) {
+    console.log("\n");
+    terminal(prompt(""), data, commandList);
+  }
+
+  console.log("See you soon!");
+}
+
+game();
